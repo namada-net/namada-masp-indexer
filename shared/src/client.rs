3a8b192face73cbd8947e3a2f50b268c176e51c7
Tiago Carvalho <sugoiuguu@tfwno.gf>
2025-09-17 13:02:01 +0100

    add user agent string to http client

--- shared/src/client.rs
+++ shared/src/client.rs
@@ -3,6 +3,7 @@ use std::time::Duration;
 use tendermint_rpc::HttpClient;
 
 const DEFAULT_REQUEST_TIMEOUT: Duration = Duration::from_secs(5);
+const DEFAULT_USER_AGENT_PREFIX: &str = "Namada Masp Indexer";
 
 #[derive(Clone, Debug)]
 pub struct Client {
@@ -15,6 +16,7 @@ impl Client {
         let inner = reqwest::Client::builder()
             .cookie_store(true)
             .timeout(DEFAULT_REQUEST_TIMEOUT)
+            .user_agent(user_agent_string())
             .build()
             .expect("Failed to create HTTP client");
         let http_client = HttpClient::new_from_parts(
@@ -34,4 +36,8 @@ impl AsRef<HttpClient> for Client {
     fn as_ref(&self) -> &HttpClient {
         &self.inner
     }
+}
+
+fn user_agent_string() -> String {
+    format!("{DEFAULT_USER_AGENT_PREFIX}{}", env!("CARGO_PKG_VERSION"))
 }
